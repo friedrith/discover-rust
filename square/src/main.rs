52a3befdb5186882c806cbfd33babd03b30797d0
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-07-25 08:52:20 -0400

    feat: make the project more stable

--- square/src/main.rs
+++ square/src/main.rs
@@ -1,21 +1,24 @@
 use std::io;
 
-fn main() {
-    println!("Please enter a number.");
+fn read_number_from_input() -> u32 {
+    loop {
+        println!("Please enter a number.");
 
-    let mut number_input = String::new();
+        let mut number_input = String::new();
 
-    io::stdin()
-        .read_line(&mut number_input)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut number_input)
+            .expect("Failed to read line");
 
-    let number: u32 = match number_input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid input, please enter a valid number.");
-            return;
+        match number_input.trim().parse() {
+            Ok(num) => return num,
+            Err(_) => println!("Invalid input, please enter a valid number."),
         }
-    };
+    }
+}
+
+fn main() {
+    let number = read_number_from_input();
 
     if number == 4 {
         println!("Sorry it does not work")
